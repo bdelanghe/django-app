c0590fd13e72f1238f8e8ee243a1e681dd812bb2
Robert DeLanghe <bdelanghe@gmail.com>
2021-03-01 17:25:46 -0500

    formated with black

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -5,29 +5,50 @@ import django.db.models.deletion
 
 from typing import List, Tuple
 
+
 class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies: List[Tuple[str, str]] = [
-    ]
+    dependencies: List[Tuple[str, str]] = []
 
     operations = [
         migrations.CreateModel(
-            name='Question',
+            name="Question",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('question_text', models.CharField(max_length=200)),
-                ('pub_date', models.DateTimeField(verbose_name='date published')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("question_text", models.CharField(max_length=200)),
+                ("pub_date", models.DateTimeField(verbose_name="date published")),
             ],
         ),
         migrations.CreateModel(
-            name='Choice',
+            name="Choice",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('choice_text', models.CharField(max_length=200)),
-                ('votes', models.IntegerField(default=0)),
-                ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("choice_text", models.CharField(max_length=200)),
+                ("votes", models.IntegerField(default=0)),
+                (
+                    "question",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="polls.question"
+                    ),
+                ),
             ],
         ),
     ]
--- polls/models.py
+++ polls/models.py
@@ -8,17 +8,19 @@ from django.utils import timezone
 _MAX_LENGTH: Final = 200
 _STARTING_VOTES: Final = 0
 
+
 @final
 class Question(models.Model):
     question_text = models.CharField(max_length=_MAX_LENGTH)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self) -> str:
         return self.question_text
 
     def was_published_recently(self) -> bool:
-        recent: bool = (self.pub_date >= timezone.now())
+        recent: bool = self.pub_date >= timezone.now()
         return recent
+
 
 @final
 class Choice(models.Model):
